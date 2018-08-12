12d0520650ad7802d9ca58887634c9b725533f98
Florian Privé <florian.prive.21@gmail.com>
2018-08-12 17:55:58 +0200

    save

---
+++ tmp-tests/test-group-by.R
@@ -0,0 +1,54 @@
+library(bigdfr)
+test <- FDF(datasets::iris)
+test$address
+test2 <- test$copy()
+test2$extptr
+split(test$ind_row, pull(test, Species))
+
+
+
+rapply(list(1, list(2, 3)), function(x) x + 1, how = "replace")
+
+df <- mtcars
+ind_row <- rows_along(df) - 1L
+split1 <- split(ind_row, df$cyl)
+str(split1)
+df1 <- select(df, -cyl)
+split2 <- lapply(split1, function(ind) split(ind, f = df1$vs[ind]))
+str(split2)
+reshape2::melt(split2)
+
+
+reshape2::melt(rapply(split2, function(x) mean(x), how = "replace"))
+
+long_vec <- rep(df$cyl, 1000)
+microbenchmark::microbenchmark(
+  unique(long_vec),
+  as.factor(long_vec),
+  split(seq_along(long_vec), long_vec)
+)
+
+
+long_vec <- rep(df$cyl, 100000)
+profvis::profvis({split(seq_along(long_vec), long_vec)})
+
+
+group <- function(prev, df, by_name) {
+
+  prev %>%
+    mutate(NESTED = lapply(ind_row, function(ind) {
+      by <- df[[by_name]][ind + 1L]
+      u_by <- unique(by)
+      tibble(!!sym(by_name) := u_by,
+             ind_row = lapply(u_by, function(x) ind[by == x]))
+    })) %>%
+    select(-ind_row) %>%
+    tidyr::unnest(NESTED)
+}
+(first <- tibble(ind_row = list(ind_row)))
+(second <- group(first, df, "cyl"))
+(third <- group(second, df, "vs"))
+(fourth <- group(third, df, "am"))
+
+mtcars %>%
+  group_by()
